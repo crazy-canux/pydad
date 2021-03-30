1b6415bff45eebe02526474434864f326faec77f
Canux <canux.cheng@arm.com>
2021-03-30 02:58:12 -0400

    bugfix

--- tests/test_search.py
+++ tests/test_search.py
@@ -4,25 +4,27 @@ from pydad.search.linear import linear
 from pydad.search.binary import binary
 from pydad.search.interpolation import interpolation
 
-seq1 = [23, 78, -25, 82, 0, 396]
 
 
 class SearchTestCase(unittest.TestCase):
     def test_linear(self):
+        seq = [23, 78, -25, 82, 0, 396]
         self.assertEqual(
-            linear(seq1, 0), 4,
+            linear(seq, 0), 4,
             msg="find 0 failed."
         )
 
     def test_binary(self):
+        seq = [-25, 0, 23, 82, 396]
         self.assertEqual(
-            binary(seq1, 0), 4,
+            binary(seq, 0), 1,
             msg="find 0 failed."
         )
 
     def test_interpolation(self):
+        seq = [-25, 0, 23, 82, 396]
         self.assertEqual(
-            interpolation(seq1, 0), 4,
+            interpolation(seq, 0), 1,
             msg='find 0 failed.'
         )
 
