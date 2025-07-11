21c52f832002f286861bd83ca9061970c014bd6f
Canux CHENG <canuxcheng@gmail.com>
2025-07-11 15:41:46 +0800

    0711 update

--- pydad/interview/1.py
+++ pydad/interview/1.py
@@ -4,8 +4,14 @@ two number sum is target
 """
 from typing import List
 
-
 def sum(seq: List[int], target: int) -> List[int]:
+    for i in range(len(seq)-1):
+        for j in range(i+1, len(seq)):
+            if seq[i] + seq[j] == target:
+                return i, j
+    return -1, -1
+
+def sum_v1(seq: List[int], target: int) -> List[int]:
     for i in range(len(seq)-1):
         rest = target - seq[i]
         if rest in seq[i+1:]:
@@ -15,3 +21,4 @@ def sum(seq: List[int], target: int) -> List[int]:
 
 if __name__ == "__main__":
     print(sum([1,2,4,6,8], 10))
+    print(sum_v1([1,2,4,6,8], 10))
