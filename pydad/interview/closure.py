2642d0fb2019724fce34f8751afc23d975accf40
Canux Cheng <canuxcheng@gmail.com>
2025-08-09 21:28:26 +0800

    0809 update

--- pydad/interview/closure.py
+++ pydad/interview/closure.py
@@ -1,15 +1,16 @@
-def outer():
-    # data = []
-    v1 = 10
+def outer(arg):
+    print("outer called")
+    v1 = arg
+    print(f"outer v1: {v1}")
     def inner(value):
-        # data.append(value)
         nonlocal v1
+        print("inner called: ", v1)
         v1 += 1
-        # return data
+        print(f"inner v1: {v1}")
         return v1
     return inner
 
 if __name__ == "__main__":
-    call = outer()
+    call = outer(10)
     print(call(1))
     print(call(2))
